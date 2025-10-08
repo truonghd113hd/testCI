21d908078cd168b54949df06cb539496171511d5
truonghd113hd <truonghd1994@gmail.com>
2025-10-08 07:17:50 +0700

    update

--- scripts/test.js
+++ scripts/test.js
@@ -6,9 +6,9 @@ fetch('https://dev.zenia.network/login', {
     'content-type': 'text/plain;charset=UTF-8',
     'next-action': '7fb96c047931578a3376831220b53ae65fd6500c5c',
     'next-router-state-tree':
-      '%5B%22%22%2C%7B%22children%22%3A%5B%22(login-template)%22%2C%7B%22children%22%3A%5B%22login%22%2C%7B%22children%22%3A%5B%22__PAGE__%22%2C%7B%7D%2Cnull%2Cnull%5D%7D%2Cnull%2Cnull%5D%7D%2Cnull%2Cnull%5D%7D%2Cnull%2Cnull%2Ctrue%5D',
+      '%5B%22%22%2C%7B%22children%22%3A%5B%22(login-template)%22%2C%7B%22children%22%3A%5B%22login%22%2C%7B%22children%22%3A%5B%22__PAGE__%22%2C%7B%7D%2Cnull%2Cnull%5D%7D%2Cnull%2Cnull%5D%7D%2Cnull%2Cnull%2Ctrue%5D%7D%2Cnull%2Cnull%2Ctrue%5D',
     priority: 'u=1, i',
-    'sec-ch-ua': '"Chromium";v="140", "Not=A?Brand";v="24", "Google Chrome";v="140"',
+    'sec-ch-ua': '"Google Chrome";v="141", "Not?A_Brand";v="8", "Chromium";v="141"',
     'sec-ch-ua-mobile': '?0',
     'sec-ch-ua-platform': '"macOS"',
     'sec-fetch-dest': 'empty',
